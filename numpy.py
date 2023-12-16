49852215da59c167f2d83d31129c116b8ad7c354
AliAlhmodi <145494212+AliAlhmodi@users.noreply.github.com>
2023-12-16 12:50:23 +0300

    Add files via upload

---
+++ numpy.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec  1 14:32:07 2023
+
+@author: admin
+"""
+
+
+    
+
+
+import numpy as n
+
+
+ls=[[5,6,8],[3,9,10],[9,6,7]]
+#lsnp=n.array(ls)
+print(ls)
+
+print('\n')
+#print(lsnp)
+
+m=n.array(ls)
+print(m)
+b=n.range(a.shape[0])
+print( b)
+
+'''
+z=np.zeros((3,4))
+print(z)
+'''
+'''
+z=np.ones((4,4))
+print(z)
+'''
+'''
+r=np.random.rand(3)
+print(r)
+'''
+'''
+r=n.random.randint(100)
+print(r)
+'''
+
+
+'''
+u=np.random.randint(100)
+print(u)
+'''
+'''
+u=np.random.randint(100,200,(5,4))
+print(u)
+'''
+
+'''u=np.random.randint(100,200,10)
+print(u)
+
+shape=u.reshape(5,2)
+print(shape)'''
